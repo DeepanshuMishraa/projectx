24e7a2901c771184b98d3a48dfead5124f30e8ad
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-01-02 16:41:09 +0530

    remove unused

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat, Poppins } from "next/font/google";
+import {  Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "./theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const pop = Poppins({
   subsets: ["latin"],
