8bafec59d05498c090dd55e420ddade9d4193103
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-01-12 22:49:36 +0530

    ui bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MainCard from "@/components/MainCard";
 
 export default function Home() {
   return (
-    <main className="flex mt-10 items-center  justify-center h-screen">
+    <main>
       <MainCard />
     </main>
   );
